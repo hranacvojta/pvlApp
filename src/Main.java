22d5a96422ec934ca01ff0ec4f10564e697d9e55
hranacvojta <hranacvojta@gmail.com>
2026-05-20 12:47:53 +0200

    testes

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import API.ContentExtractor;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
@@ -6,41 +7,11 @@ import static API.ConnetionManager.PvlConnector.scrape;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//        TitleScreen ts = new TitleScreen();
-//        ts.init();
+        Screens.TitleScreen ts = new Screens.TitleScreen();
+        ts.init();
         Document document = scrape("https://www.pvl.cz/portal/sap/cz/pc/");
+        ContentExtractor  contentExtractor = new ContentExtractor();
+        contentExtractor.extractContent(document);
 
     }
 }
-//
-//        // 1. Tohle je náš surový HTML kód (takhle nějak by vypadal stažený z internetu)
-//        String htmlZWebu =
-//                "<html>" +
-//                        "  <body>" +
-//                        "    <div class='produkt'>" +
-//                        "      <h1 class='nazev'>Kniha: Java pro začátečníky</h1>" +
-//                        "      <p>Popis: Nejlepší kniha na trhu.</p>" +
-//                        "      <span class='cena'>599 Kč</span>" +
-//                        "    </div>" +
-//                        "  </body>" +
-//                        "</html>";
-//
-//        // 2. Předáme to Jsoupu, ať to "přežvýká" do svého stromu (Document)
-//        Document doc = Jsoup.parse(htmlZWebu);
-//
-//        // 3. DOLOVÁNÍ DAT:
-//        // Řekneme: "Najdi prvek s třídou 'nazev' a dej mi z něj jen čistý text"
-//        String vytezeneJmeno = doc.select(".nazev").text();
-//
-//        // Řekneme: "Najdi prvek s třídou 'cena' a dej mi z něj jen čistý text"
-//        String vytezenaCena = doc.select(".cena").text();
-//
-//        // 4. Výpis do konzole
-//        System.out.println("Úspěšně vykucháno z HTML!");
-//        System.out.println("Název produktu: " + vytezeneJmeno);
-//        System.out.println("Cena produktu: " + vytezenaCena);
-//    }
-//}
-
-
-
