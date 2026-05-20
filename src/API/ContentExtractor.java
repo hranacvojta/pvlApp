2e0d2fd800723555bf1e0f556395e0fd0db01bf8
hranacvojta <hranacvojta@gmail.com>
2026-05-20 12:47:16 +0200

    ContentExtractor is used for parsing html into json file

---
+++ src/API/ContentExtractor.java
@@ -0,0 +1,15 @@
+package API;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import javax.net.ssl.SSLSocketFactory;
+import java.io.IOException;
+
+public class ContentExtractor {
+
+    public static void extractContent(Document document){
+        //Title extract
+        System.out.println(document);
+
+    }
+}
