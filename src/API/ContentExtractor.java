7d7546b08ade7e56cb583e0bd2383e7172ead6a4
hranacvojta <hranacvojta@gmail.com>
2026-05-27 22:27:58 +0200

    start up logic for content extraction

--- src/API/ContentExtractor.java
+++ src/API/ContentExtractor.java
@@ -1,15 +1,34 @@
 package API;
+import API.ConnetionManager.PvlConnector;
+import com.google.gson.Gson;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-import javax.net.ssl.SSLSocketFactory;
 import java.io.IOException;
+
 
 public class ContentExtractor {
 
-    public static void extractContent(Document document){
-        //Title extract
-        System.out.println(document);
+
+    public static void extractContent(String url) throws IOException {
+        Document doc = null;
+
+        Gson gson = new Gson();
+        try {
+            doc = PvlConnector.scrape(url);
+        }catch (Exception e){
+            System.err.println("Nepodařilo se spojit s webem: " + e.getMessage());
+            return;
+        }
+
+        if (doc == null){
+            System.out.println("Theres is no data(html) to process");
+            return;
+        }
+
+        //     TEST
+//        System.out.println(PvlConnector.scrape(url));
+
 
 
     }
