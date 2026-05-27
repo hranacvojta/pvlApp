8607f81ef24c22040103c0bd7d1ac311c56e16c4
hranacvojta <hranacvojta@gmail.com>
2026-05-27 22:30:15 +0200

    testes

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,14 @@
+import API.ConnetionManager.PvlConnector;
 import API.ContentExtractor;
-import org.jsoup.nodes.Document;
 
 import java.io.IOException;
 
-import static API.ConnetionManager.PvlConnector.scrape;
-
 public class Main {
     public static void main(String[] args) throws IOException {
-        Screens.TitleScreen ts = new Screens.TitleScreen();
-        ts.init();
-        Document document = scrape("https://www.pvl.cz/portal/sap/cz/pc/");
-        ContentExtractor  contentExtractor = new ContentExtractor();
-        contentExtractor.extractContent(document);
+//        Screens.TitleScreen ts = new Screens.TitleScreen();
+//        ts.init();
 
+        String goalAdress = "https://www.pvl.cz/portal/sap/cz/pc/";
+        ContentExtractor.extractContent(goalAdress);
     }
 }
