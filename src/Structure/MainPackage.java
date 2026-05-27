0584f6dde680d4a09072784477ae9b4ba2e72032
hranacvojta <hranacvojta@gmail.com>
2026-05-27 22:27:01 +0200

    class changed from common class to record

--- src/Structure/MainPackage.java
+++
@@ -1,9 +0,0 @@
-package Structure;
-import java.util.ArrayList;
-import java.util.List;
-
-public class MainPackage {
-
-    public Metadata metadata;
-    public List<Station> stanice = new ArrayList<>();
-}
---
+++ src/Structure/StructureCase.java
@@ -0,0 +1,5 @@
+package Structure;
+
+import java.util.List;
+
+public record StructureCase(Metadata metadata, List<Station> stations){}
